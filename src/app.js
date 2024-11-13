74d5f24d59d78712bb22a28d3a3344daae32992d
yaaqout <27elruby@gmail.com>
2024-11-13 10:16:20 +0200

    first commit

---
+++ src/app.js
@@ -0,0 +1,51 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT ||3000
+
+
+const path = require('path')
+
+const hbs = require('hbs')
+app.set('view engine','hbs')
+
+const partialPath = path.join(__dirname, '../partials')
+hbs.registerPartials(partialPath)
+
+app.use(express.static('public'))
+
+// ----------- #Pages . ------------ 
+
+app.get('/',(req,res)=>{
+  res.render('index',{
+    title: "Home Page Content"
+  })
+})
+app.get('/about',(req,res)=>{
+  res.render('about',{
+    title: "About Page Content"
+  })
+})
+app.get('/contact',(req,res)=>{
+  res.render('contact',{
+    title: "Contact Page Content"
+  })
+})
+app.get('/product',(req,res)=>{
+  res.render('product',{
+    title: "Product Page Content"
+  })
+})
+app.get('/service',(req,res)=>{
+  res.render('service',{
+    title: "Service Page Content"
+  })
+})
+
+
+
+
+
+app.listen(port , ()=> {                          
+  console.log(`app is listening on port ${port}`)     
+  })
